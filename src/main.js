fffa9a27b820c57b8c110949b8ca63c3a13a000f
Jan Bárta <jbarta.edu@gmail.com>
2020-08-26 14:27:25 +0200

    Added functions to utils, make main more clean

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 const Apify = require('apify');
+const { utils: { log } } = Apify;
 
-const iterationFn = require('./iteration-fn.js');
-const loadAndProcessResults = require('./load-and-process.js');
-const { prepareOutput } = require('./utils.js');
+const { validateInput, prepareOutput, loadAndProcessResults,
+        iterationFunction, getEvaluatedCheckFncOrThrow,
+        getDatasetItemCountOrThrow, loadDataFromStoreOrThrow } = require('./utils.js');
 
 Apify.main(async () => {
     const input = await Apify.getInput();
-    console.log('input');
+    log.info('input:');
     console.dir(input);
 
     const {
@@ -32,44 +33,24 @@ Apify.main(async () => {
         minDuplications,
     };
 
-    if (!field) {
-        throw new Error('Input should contain: "field"!');
-    }
-    if (!datasetId && !keyValueStoreRecord && !rawData) {
-        throw new Error('Input should contain at least one of: "apifyStorageId", "keyValueStoreRecord" or "rawData"!');
-    }
-    const providedDataInputs = [datasetId, rawData, keyValueStoreRecord].filter((dataInput) => !!dataInput);
-    if (providedDataInputs.length > 1) {
-        throw new Error('Input cannot contain more than one of: "apifyStorageId", "keyValueStoreRecord" or "rawData"! ');
-    }
-    if (keyValueStoreRecord && keyValueStoreRecord.split('+').length !== 2) {
-        throw new Error('Could not parse key value store ID and record key from "keyValueStoreRecord"!')
-    }
-    let preCheckFunctionEvaled;
+    // input validation
+   await validateInput(input);
+
+   let pareCheckFunctionEvaluated;
     if (preCheckFunction) {
-        try {
-            preCheckFunctionEvaled = eval(preCheckFunction);
-        } catch (e) {
-            throw new Error('Evaluating "preCheckFunction" failed, please inlcude valid javascript! Error:', e);
-        }
-        if (typeof preCheckFunctionEvaled !== 'function') {
-            throw new Error(`"preCheckFunction" should be a function! Instead it is ${typeof preCheckFunctionEvaled}`);
-        }
+        pareCheckFunctionEvaluated = await getEvaluatedCheckFncOrThrow(preCheckFunction);
     }
 
     const state = await Apify.getValue('STATE');
-    const duplicatesState = state
-        ? state.duplicatesState
-        : {};
+    const duplicatesState = state ? state.duplicatesState : {};
 
     if (datasetId) {
-        const datasetInfo = await Apify.client.datasets.getDataset({ datasetId })
-            .catch(() => { throw new Error(`Dataset with "datasetId": "${datasetId}" was not found!`); });
-        const totalItemCount = datasetInfo.itemCount;
-        console.log('Total items in dataset:', totalItemCount);
-        await loadAndProcessResults({
-            iterationFn,
-            preCheckFunction: preCheckFunctionEvaled,
+        const totalItemCount = await getDatasetItemCountOrThrow(datasetId);
+        const actualOffset = state ? state.offset : offset;
+        const outputOffset = state ? state.outputOffset : 0;
+
+        const processOptions = {
+            preCheckFunction: pareCheckFunctionEvaluated,
             datasetId,
             batchSize,
             limit: limit || totalItemCount,
@@ -77,16 +58,14 @@ Apify.main(async () => {
             field,
             showOptions,
             checkOnlyCleanItems
-        },
-        state ? state.offset : offset, state ? state.outputOffset : 0);
+        };
+
+        await loadAndProcessResults(processOptions, actualOffset, outputOffset);
     } else {
         // KV store or rawData path
         let data;
         if (keyValueStoreRecord) {
-            const [storeId, key] = keyValueStoreRecord.split('+');
-            data = await Apify.client.keyValueStores.getRecord({ storeId, key })
-                .then((res) => res.body)
-                .catch(() => { throw new Error(`Key-value store record with "ID": "${storeId}" and "recordKey": "${key}" was not found! Please input correct storage.`); });
+            data = await loadDataFromStoreOrThrow(keyValueStoreRecord);
         } else if (rawData) {
             data = rawData;
         }
@@ -94,14 +73,17 @@ Apify.main(async () => {
         if (!Array.isArray(data)) {
             throw new Error('Data loaded from key value store must be an array!');
         }
-        console.log(`Total items loaded: ${data.length}`);
-        const duplicateItems = iterationFn({
+        log.info(`Total items loaded: ${data.length}`);
+
+        const iterationFunctionOptions = {
             items: data,
-            preCheckFunction: preCheckFunctionEvaled,
+            preCheckFunction: pareCheckFunctionEvaluated,
             duplicatesState,
             field,
             showOptions,
-        });
+        };
+        const duplicateItems = iterationFunction(iterationFunctionOptions);
+
         if (showItems) {
             await Apify.pushData(duplicateItems);
         }
